7b2ce9e6cbb2d3e7392b6bd1400e8b63e82c62df
spalato <7659022+spalato@users.noreply.github.com>
2025-03-21 11:01:26 +0100

    Surrogate now using scaled coordinates.

--- try_surrogates.jl
+++ try_surrogates.jl
@@ -52,10 +52,15 @@ function main()
     # bounds for parameters
     lb = [0.0, 0.01]
     ub = [10.0, 0.1]
+    scales = ub .- lb
+    lbs = lb./scales
+    ubs = ub./scales
+    guess_s = guess ./ scales
+    scaled_ssq(p) = ssq(p.*scales)
     nsamples = 25
     sampling = SobolSample()
-    xsamp = sample(nsamples, lb, ub,SobolSample())
-    ysamp = ssq.(xsamp)
+    xsamp = sample(nsamples, lbs, ubs, SobolSample())
+    ysamp = scaled_ssq.(xsamp)
     #@info "Initial sampling" xsamp ysamp
     # Radial (default): bad
     # Kriging (default): smooth
@@ -64,37 +69,39 @@ function main()
     # Wendland Completely off.
     # Lobachevsky with alpha=1.0, 10 seems to do it. That would be 1/scale, more or less.
     #surrogate = Kriging(xsamp, ysamp, lb, ub, p=[1.5, 1.5], theta=[0.1, 1.0])#[1/(u-l) for (l, u) in zip(lb, ub)]) 
-    surrogate = Kriging(xsamp, ysamp, lb, ub, p=[1.5, 1.5], theta=[0.01, 1.0])
+    surrogate = Kriging(xsamp, ysamp, lbs, ubs, p=[1.5, 1.5], theta=[10.0, 10.0])
     # Lobachevsky seems to crash on a linear algebra problem (like adding the same point many times).
     #surrogate = LobachevskySurrogate(xsamp, ysamp, lb, ub, alpha=[0.01, 0.01]) 
     #surrogate = PolynomialChaosSurrogate(xsamp, ysamp, lb, ub)
-    @info "Estimation, at guess" surrogate(guess) ssq(guess)
+    @info "Estimation, at guess" surrogate(guess_s) ssq(guess)
     
     # compute the rms distance between the true result and the surrogate over initial samples
-    @info "Initial RMS error" sum(@. (surrogate(xsamp) - ysamp)^2)
+    #@info "Initial RMS error" sum(@. (surrogate(xsamp) - ysamp)^2)
     # plot the true surface, the initial surrogate and the final surrogate.
     x_smooth = range(lb[1], ub[1], 64)
     y_smooth = range(lb[2], ub[2], 64)
     levels = 0:5:50
-    surf_true = contourf(x_smooth, y_smooth, (x, y)->ssq([x y]), levels=levels)
+    #@infiltrate
+    #surf_true = contourf(x_smooth, y_smooth, (x, y)->ssq([x y]), levels=levels)
+    surf_scaled = contourf(x_smooth./scales[1], y_smooth./scales[2], (x, y)->scaled_ssq((x, y)), levels=levels)
 
-    surf_init = contourf(x_smooth, y_smooth, (x, y)->surrogate([x y]), levels=levels)
+    surf_init = contourf(x_smooth/scales[1], y_smooth/scales[2], (x, y)->surrogate([x y]), levels=levels)
     p1s = [xy[1] for xy in xsamp]
     p2s = [xy[2] for xy in xsamp]
     scatter!(surf_init, p1s, p2s,  markercolor=:white, cbar=false)
     
  #   # optimizing
-    sur_res = surrogate_optimize(ssq, EI(), lb, ub, surrogate, sampling)
+    sur_res = surrogate_optimize(scaled_ssq, EI(), lbs, ubs, surrogate, sampling)
     @info "Surrogate optimize complete" sur_res[1] sur_res[2]
-    surf_end = contourf(x_smooth, y_smooth, (x, y)->surrogate([x y]), levels=levels)
+    surf_end = contourf(x_smooth/scales[1], y_smooth/scales[2], (x, y)->surrogate([x y]), levels=levels)
     p1s = [xy[1] for xy in xsamp]
     p2s = [xy[2] for xy in xsamp]
     scatter!(surf_end, p1s, p2s, markercolor=:white, cbar=false)
-    surfs = [surf_true, surf_init, surf_end]
+    surfs = [surf_scaled, surf_init, surf_end]
     for s in surfs
-        scatter!(s, [guess[1]], [guess[2]], markercolor=:red)
-        scatter!(s, [5.0], [1/20.0], markercolor=:blue)
+        scatter!(s, [guess_s[1]], [guess_s[2]], markercolor=:red)
+        scatter!(s, [5.0/scales[1]], [1/20.0/scales[2]], markercolor=:blue)
     end
-    display(plot(surfs..., layout=(1,3), size=(1200, 300), legend=false))
+    display(plot(surfs..., layout=(1,length(surfs)), size=(1200, 300), legend=false))
     @info "Sample length from .. to " nsamples length(xsamp)
 end
