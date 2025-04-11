c4b1df4dff48bd73aba59961c2509d96807f5015
spalato <7659022+spalato@users.noreply.github.com>
2025-04-11 12:28:45 +0200

    Fixed a bug I had just introduced.

--- test/tests_datastore.jl
+++ test/tests_datastore.jl
@@ -11,11 +11,12 @@ cases = [
 
 for (name, params, value) in cases
     # testing against hard coded values to ensure it does not depend on julia
-    # version or invokation
+    # version or invocation
     @test hash_call(params) == value
 end
 
 
+# From: https://discourse.julialang.org/t/macro-for-counting-the-number-of-times-a-function-is-called/3129
 mutable struct Counting{TF}
     f::TF
     counter::Int
@@ -28,6 +29,11 @@ function (c::Counting)(args...)
     c.counter += 1
     c.f(args...)
 end
+
+# Define test functions
+f1(x, y) = x + y
+f2(x, y, z) = x * y * z
+f3(x) = x^2
 
 @testset "stored function tests" begin
     # Create a temporary HDF5 file
@@ -52,9 +58,8 @@ end
                 @test haskey(group, hash_call(p))  # Ensure result is cached
                 @test get_result(group, hash_call(p)) == r
                 @test get_param(group, hash_call(p)) == p
-
                 
-                count = counted_f.counter  # Ensure our counter was reset.
+                count = counted_f.counter  # Grab current execution count
                 @test stored_f(p...) == r
                 @test counted_f.counter == count  # Ensure no recomputation
             end
